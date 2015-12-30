d6948a2890941432ba4f756f639a156d0d039afb
saisuniltata <saisuniltata@gmail.com>
2015-12-29 21:42:27 -0600

    hello world

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
-var express = require('express');
-var app = express();
-var http = require('http');
-
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
-
-
-http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
-    console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
-    server();
+var port = process.env.OPENSHIFT_INTERNAL_PORT;
+var ip = process.env.OPENSHIFT_INTERNAL_IP;
+var connect = require('connect');
+var app = connect().use(function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello World\n');
 });
+connect.createServer(app).listen(port, ip);
