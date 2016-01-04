c84f790824930f5fdf9cc0059da08f9f5f560dd1
saisuniltata <saisuniltata@gmail.com>
2016-01-04 17:26:42 -0600

    express send file test14

--- server.js
+++ server.js
@@ -9,76 +9,7 @@ io = require('socket.io').listen(server);
 server.listen(port,ip);
 
 app.get('/',function(req,res){
-    res.send('<!doctype html>
-<html lang="en">
-<head>
-<title>Chat</title>
-<style>
-#mainWrapper{
-    
-}
-
-#namesWrapper{
-    display:none;
-}
-
-#userWrapper{
-
-}
-</style>
-<meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-</head>
-
-<body>
-    <div id = "container">
-        <div id = "namesWrapper">
-            <div class="col-xs-3">
-                <div class="panel panel-primary">
-                     <div class="panel-heading">Chat App</div>
-                 
-                        <form id ="userName">
-                            <div class="panel-body" >Username
-                            <br/><br/>
-                            <input size = "10" class="form-control" id="username"  ><br/>
-                            <input type="submit" class="btn btn-info" value="Submit"><br/>
-                            </div>
-                        </form> 
-                    </div>
-                </div>
-            </div>
-            
-        </div>
-
-        <div id = "mainWrapper">
-            <div class="col-xs-6">
-                <div class="panel panel-primary">
-                     <div class="panel-heading">Chat App</div>
-                                <div id = "userWrapper">
-                                    <div id = "users">
-                        
-                                    </div>
-                                <div id = "chatWrapper">
-                                    <form id = "messageForm"
-                                    <div class="panel-body" ></br>
-                                        <div class="col-xs-8">
-                                        <textarea class="form-control" rows="10" id="message"></textarea><br/>
-                                        <input type="submit" class="btn btn-info" value="Submit">
-                                        </div>
-                                    </form>
-                                </div>  
-                                </div>
-                                
-                        </div>
-                </div>
-            </div>
-        </div>  
-    </div>
-</body>
-<html>');
+    res.send('<p>hello world</p>');
 });
 
 io.sockets.on('connection',function(data){
