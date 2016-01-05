3127fa1a6627d9dcef3ac9ba0a2b97eb16fe50e8
saisuniltata <saisuniltata@gmail.com>
2016-01-04 19:02:53 -0600

    Socket test1

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.get('/',function(req,res){
 
 server.listen(port,ip);
 
-io.sockets.on('connection',function(socket){
+/*io.sockets.on('connection',function(socket){
     socket.on('send message',fucntion(data){
         io.sockets.emit('new message',(msg: data));
     });
-});
+});*/
