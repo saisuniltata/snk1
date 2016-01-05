f7f824dd8dd1ca5915150110e8391413d172fe7a
saisuniltata <saisuniltata@gmail.com>
2016-01-04 19:43:16 -0600

    Socket test4

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var port = process.env.OPENSHIFT_NODEJS_PORT||8080;
 var ip = process.env.OPENSHIFT_NODEJS_IP||'127.0.0.1';
 app = express(),
 server = require('http').createServer(app),
-io = require('socket.io').listen(server);
+io = require('socket.io')(server);
 
 
 app.use('/',express.static('static'));
